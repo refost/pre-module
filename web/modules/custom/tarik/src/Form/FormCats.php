3a1fcd0eff9030fc297da3ddee11f6969f9d1cd4
refost <refost220@gmail.com>
2021-07-21 16:58:58 +0300

    field for email and validation was added

--- web/modules/custom/tarik/src/Form/FormCats.php
+++ web/modules/custom/tarik/src/Form/FormCats.php
@@ -32,6 +32,23 @@ class FormCats extends FormBase {
       '#maxlength' => 32,
     ];
 
+    $form['email'] = [
+      '#type' => 'email',
+      '#title' => $this->t('Your email:'),
+      '#placeholder' => $this->t('your@mail.com'),
+      '#required' => TRUE,
+      '#pattern' => '/[-_aA-zZ]{2,30}@[a-z]{2,10}\.[a-z]{2,10}/',
+      '#ajax' => [
+        'callback' => '::validSymb',
+        'event' => 'keyup',
+      ],
+    ];
+
+    $form['email-valid'] = [
+      '#type' => 'markup',
+      '#markup' => '<div id="valid_message"></div>',
+    ];
+
     $form['submit'] = [
       '#type' => 'submit',
       '#value' => $this->t('Add cat'),
@@ -62,10 +79,40 @@ class FormCats extends FormBase {
   }
 
   /**
+   * Symbols input validation.
+   */
+  public function validSymb(array &$form, FormStateInterface $form_state):object {
+    $regular = '/[-_@aA-zZ.]/';
+    $line = $form_state->getValue('email');
+
+    $response = new AjaxResponse();
+
+    for ($i = 0; $i < strlen($line); $i++) {
+      if (!preg_match($regular, $line[$i])) {
+        $response->addCommand(
+          new HtmlCommand(
+            '#valid_message',
+            '<div class="invalid-message">' . $this->t('You can use only letters and "-" or "_" or "@"')
+          )
+        );
+        break;
+      }
+      else {
+        $response->addCommand(
+          new HtmlCommand(
+            '#valid_message',
+            ''
+          )
+        );
+      }
+    }
+    return $response;
+  }
+
+  /**
    * Function for message with info what was sended.
    */
   public function setMessage(array &$form, FormStateInterface $form_state):object {
-
     $response = new AjaxResponse();
     if (2 > strlen($form_state->getValue('name'))) {
       $response->addCommand(
@@ -75,11 +122,21 @@ class FormCats extends FormBase {
         )
       );
     }
+    elseif (!preg_match('/[-_aA-zZ]{2,30}@[a-z]{2,10}\.[a-z]{2,10}/', $form_state->getValue('email'))) {
+      $response->addCommand(
+        new HtmlCommand(
+          '#result_message',
+          '<div class="form-cat-message">' . $this->t('Your email is not supported')
+        )
+      );
+    }
     else {
       $response->addCommand(
         new HtmlCommand(
           '#result_message',
-          '<div class="form-cat-message">' . $this->t('Thanks for sending. The name of cat is @result', ['@result' => ($form_state->getValue('name'))])
+          '<div class="form-cat-message">' .
+          $this->t('Thanks for sending. The name of cat is @result',
+            ['@result' => ($form_state->getValue('name'))])
         )
       );
     }
